916966d2fc1b18a5db95ea15b59752a085434c95
Mr.Shankar Aryal <shankararyal737@gmail.com>
2024-10-29 19:04:15 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,3 @@ for (let i = 0; i < navbarLinks.length; i++) {
     navToggleBtn.classList.toggle("active");
   });
 }
-const backTopBtn = document.querySelector("[data-back-to-top]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
