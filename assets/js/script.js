564e4aae57cf06b98022ca20e5a88a961344b762
Mr.Shankar Aryal <shankararyal737@gmail.com>
2024-10-30 23:11:16 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,1 @@
-'use strict';
-const header = document.querySelector("[data-header]");
-const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
-
-navToggleBtn.addEventListener("click", function () {
-  header.classList.toggle("nav-active");
-  this.classList.toggle("active");
-});
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    header.classList.toggle("nav-active");
-    navToggleBtn.classList.toggle("active");
-  });
-}
+"use strict";const header=document.querySelector("[data-header]"),navToggleBtn=document.querySelector("[data-nav-toggle-btn]");navToggleBtn.addEventListener("click",(function(){header.classList.toggle("nav-active"),this.classList.toggle("active")}));const navbarLinks=document.querySelectorAll("[data-nav-link]");for(let e=0;e<navbarLinks.length;e++)navbarLinks[e].addEventListener("click",(function(){header.classList.toggle("nav-active"),navToggleBtn.classList.toggle("active")}));
